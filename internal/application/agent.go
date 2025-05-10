6da3be654134ed7b50b58f1b13d5055b5966eae1
Andreyka-coder9192 <andreykalinin2009@gmail.com>
2025-05-10 13:06:30 +0300

    fix 1

--- internal/application/agent.go
+++ internal/application/agent.go
@@ -1,21 +1,23 @@
 package application
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
+	"context"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/Andreyka-coder9192/calc_go/pkg/calculation"
+	"github.com/Andreyka-coder9192/calc_go/proto/calc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Agent struct {
-	ComputingPower  int
-	OrchestratorURL string
+	ComputingPower int
+	grpcClient     calc.CalcClient
 }
 
 func NewAgent() *Agent {
@@ -23,19 +25,26 @@ func NewAgent() *Agent {
 	if err != nil || cp < 1 {
 		cp = 1
 	}
-	orchestratorURL := os.Getenv("ORCHESTRATOR_URL")
-	if orchestratorURL == "" {
-		orchestratorURL = "http://localhost:8080"
+	target := os.Getenv("ORCHESTRATOR_URL")
+	if target == "" {
+		target = "localhost:8080"
+	} else {
+		target = target[len("http://"):]
 	}
-	return &Agent{
-		ComputingPower:  cp,
-		OrchestratorURL: orchestratorURL,
+	conn, err := grpc.Dial(
+		target,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("agent: cannot connect to gRPC: %v", err)
 	}
+	client := calc.NewCalcClient(conn)
+	return &Agent{ComputingPower: cp, grpcClient: client}
 }
 
 func (a *Agent) Run() {
 	for i := 0; i < a.ComputingPower; i++ {
-		log.Printf("Starting worker %d", i)
 		go a.worker(i)
 	}
 	select {}
@@ -43,57 +52,27 @@ func (a *Agent) Run() {
 
 func (a *Agent) worker(id int) {
 	for {
-		resp, err := http.Get(a.OrchestratorURL + "/internal/task")
+		task, err := a.grpcClient.GetTask(context.Background(), &calc.Empty{})
 		if err != nil {
-			log.Printf("Worker %d: error getting task: %v", id, err)
-			time.Sleep(2 * time.Second)
+			if st, ok := status.FromError(err); ok && st.Code() == codes.NotFound {
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+			log.Printf("worker %d: GetTask error: %v", id, err)
+			time.Sleep(500 * time.Millisecond)
 			continue
 		}
-		if resp.StatusCode == http.StatusNotFound {
-			resp.Body.Close()
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		var taskResp struct {
-			Task struct {
-				ID            string  `json:"id"`
-				Arg1          float64 `json:"arg1"`
-				Arg2          float64 `json:"arg2"`
-				Operation     string  `json:"operation"`
-				OperationTime int     `json:"operation_time"`
-			} `json:"task"`
-		}
-		err = json.NewDecoder(resp.Body).Decode(&taskResp)
-		resp.Body.Close()
-		if err != nil {
-			log.Printf("Worker %d: error decoding task: %v", id, err)
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		task := taskResp.Task
-		log.Printf("Worker %d: received task %s: %f %s %f, simulating %d ms", id, task.ID, task.Arg1, task.Operation, task.Arg2, task.OperationTime)
 		time.Sleep(time.Duration(task.OperationTime) * time.Millisecond)
 		result, err := calculation.Compute(task.Operation, task.Arg1, task.Arg2)
 		if err != nil {
-			log.Printf("Worker %d: error computing task %s: %v", id, task.ID, err)
 			continue
 		}
-		resultPayload := map[string]interface{}{
-			"id":     task.ID,
-			"result": result,
-		}
-		payloadBytes, _ := json.Marshal(resultPayload)
-		respPost, err := http.Post(a.OrchestratorURL+"/internal/task", "application/json", bytes.NewReader(payloadBytes))
+		_, err = a.grpcClient.PostResult(context.Background(), &calc.ResultReq{
+			Id:     task.Id,
+			Result: result,
+		})
 		if err != nil {
-			log.Printf("Worker %d: error posting result for task %s: %v", id, task.ID, err)
-			continue
+			log.Printf("worker %d: PostResult error: %v", id, err)
 		}
-		if respPost.StatusCode != http.StatusOK {
-			body, _ := io.ReadAll(respPost.Body)
-			log.Printf("Worker %d: error response posting result for task %s: %s", id, task.ID, string(body))
-		} else {
-			log.Printf("Worker %d: successfully completed task %s with result %f", id, task.ID, result)
-		}
-		respPost.Body.Close()
 	}
 }

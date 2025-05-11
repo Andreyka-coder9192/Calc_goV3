83e42862423d8befb3ab5db5bde61789be95d015
Andreyka-coder9192 <andreykalinin2009@gmail.com>
2025-05-11 15:25:52 +0300

    fix 3

--- internal/application/orchestrator.go
+++ internal/application/orchestrator.go
@@ -363,3 +363,18 @@ func (o *Orchestrator) RunServer() error {
 	log.Println("gRPC listening on 9090")
 	return grpcSrv.Serve(lis)
 }
+
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
